5ee3538c79213733aeb4d59f54da8f387a31bf87
thiagomvn <thiagomvnn@gmail.com>
2023-03-28 00:16:29 -0300

    Commit dia 1

---
+++ dia1.py
@@ -0,0 +1,12 @@
+# Números e operadores
+a = 5
+b = 4
+soma = a + b
+
+# Estrutura condicional if/else
+if soma == 9:
+    print("A soma é igual a 9!")
+
+else:
+    print("A soma é diferente de 9!")
+    
